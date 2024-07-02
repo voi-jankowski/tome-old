685b39016aad6bfffbd38cacfa6da71b36a42749
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-02 10:04:17 +1000

    Updated data-source with entities and synchronize and logging.

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,6 +1,12 @@
 import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { User } from "./entities/User";
+import { Project } from "./entities/Project";
+import { Category } from "./entities/Category";
+import { Item } from "./entities/Item";
+import { Attribute } from "./entities/Attribute";
+import { ItemAttribute } from "./entities/ItemAttribute";
 
 const port = process.env.DB_PORT as number | undefined;
 
@@ -11,6 +17,9 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [`${__dirname}/**/entities/*.{ts,js}`],
-  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+  synchronize: false, // set to true to auto-generate tables, but only in dev mode (not for production)
+  logging: true, // set to true to log SQL queries
+  entities: [User, Project, Category, Item, Attribute, ItemAttribute],
+  migrations: [`${__dirname}/migrations/*,{ts,js}`],
+  subscribers: [],
 });
