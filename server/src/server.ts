1b1c3853bffbcfb39e9aac8eb55efe5697f6b682
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-07-03 17:38:33 +1000

    Added routes and controllers directories and updated server.ts/

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,15 @@
 import "reflect-metadata";
 import express from "express";
 import { AppDataSource } from "./data-source";
+import { router } from "./routes";
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", router);
 
 AppDataSource.initialize()
   .then(() => {
-    const app = express();
-    app.use(express.json());
-
-    app.get("/", (req, res) => {
-      res.json("Established connection!");
-    });
-
     app.listen(process.env.PORT, () => {
       console.log(`Server is running at http://localhost:${process.env.PORT}`);
     });
